89bd099eb641b745e56c82b7e5b593adbde85d3c
AbhishekSuccessive20 <abhishek.khurana@successive.tech>
2020-12-15 16:27:22 +0530

    added new files

---
+++ src/components/Slider/style.js
@@ -0,0 +1,8 @@
+const center = {
+  display: 'flex',
+  width: 'auto',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+};
+
+export default center;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -9,7 +7,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
